5a9d0cc54fb7da5d686bb8fddee1439a938c07bd
DanielMinas <daniel.mineiro10@gmail.com>
2022-07-11 18:19:24 -0300

    Implementação da Regra de negocio de inserir

--- curso-backend/src/main/java/com/projeto/curso/repository/ICursoRepository.java
+++ curso-backend/src/main/java/com/projeto/curso/repository/ICursoRepository.java
@@ -1,9 +1,14 @@
 package com.projeto.curso.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.projeto.curso.entity.Curso;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICursoRepository  extends JpaRepository<Curso, Long>{
+	
+	List<Curso> findByDataInicioLessThanEqualAndDataTerminoGreaterThanEqual(LocalDate dataInicio, LocalDate dataTermino);
 
 }
--- curso-backend/src/main/java/com/projeto/curso/service/CursoService.java
+++ curso-backend/src/main/java/com/projeto/curso/service/CursoService.java
@@ -1,6 +1,9 @@
 package com.projeto.curso.service;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,43 +18,49 @@ public class CursoService {
 
 	@Autowired
 	ICursoRepository repository;
-	
-	
-	public Curso save(Curso curso) {		
+
+	public Curso save(Curso curso) {
 		validaDataCadastro(curso);
+		validaPeridoCadastro(curso);
 		return repository.save(curso);
 	}
-
 
 	public List<Curso> findAll() {
 		// TODO Auto-generated method stub
 		return repository.findAll();
 	}
 
-
 	public Optional<Curso> findById(Long id) {
 
 		return repository.findById(id);
 	}
 
-
 	public void delete(Curso curso) {
 		validaExclusao(curso);
 		repository.delete(curso);
-		
+
 	}
+
 	public void validaDataCadastro(Curso curso) {
-		if(curso.getDataInicio().isBefore(LocalDate.now())) {
+		if (curso.getDataInicio().isBefore(LocalDate.now())) {
 			throw new RuntimeException("Data inicial deve ser superior à " + LocalDate.now());
 		}
 	}
+
 	public void validaExclusao(Curso curso) {
-		if(curso.getDataTermino().isBefore(LocalDate.now())) {
+		if (curso.getDataTermino().isBefore(LocalDate.now())) {
 			throw new RuntimeException("Não pode ser excluido, pois o curso já terminou");
 		}
 	}
 
+	public void validaPeridoCadastro(Curso curso) {
+
+		List<Curso> listaCurso = repository.findByDataInicioLessThanEqualAndDataTerminoGreaterThanEqual(curso.getDataInicio(), curso.getDataTermino());
+
+		if (listaCurso.size()> 0) {
+			throw new RuntimeException("Existe(m) curso(s) planejados(s) dentro do período informado");
+		}
 	
-	
+	}
 
 }
