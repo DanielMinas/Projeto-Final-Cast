0ddc669769e8b833ad946da7fbe9c6bc734fc469
DanielMinas <daniel.mineiro10@gmail.com>
2022-07-11 21:04:47 -0300

    Estruturação do projeto

--- curso-frontend/src/app/app.module.ts
+++ curso-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastrarCursoComponent } from './cadastrar-curso/cadastrar-curso.component';
+import { EditarCursoComponent } from './editar-curso/editar-curso.component';
+import { ConsultarCursoComponent } from './consultar-curso/consultar-curso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastrarCursoComponent,
+    EditarCursoComponent,
+    ConsultarCursoComponent
   ],
   imports: [
     BrowserModule,
